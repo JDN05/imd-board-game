657f11b8aa85e55ffa47617746e793b9676bcdbb
AmethystApples <yakseenkhan@gmail.com>
2023-11-30 06:15:10 -0800

    Testing Piece Interactions

--- imd-board-game-VR/Assets/Scripts/InputReader.cs
+++ imd-board-game-VR/Assets/Scripts/InputReader.cs
@@ -9,6 +9,7 @@ public class InputReader : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Debug.Log("hi");
         initializeInputReader();
     }
 
