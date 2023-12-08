7209dd5bd9da352829865d31cf8392745692de05
AmethystApples <yakseenkhan@gmail.com>
2023-12-08 00:02:03 -0800

    Revert "Edited Pieces to be interactable. Continue implementing Netcode"
    This reverts commit a04ae5068fb04128558262cca12fc90a47ba45d5.

--- imd-board-game-VR/Assets/NetworkConnect.cs
+++ imd-board-game-VR/Assets/NetworkConnect.cs
@@ -2,42 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Unity.Netcode;
-using Unity.Services.Authentication;
-using Unity.Services.Core;
-using Unity.Services.Relay;
-using Unity.Services.Relay.Models;
-using Unity.Netcode.Transports.UTP;
-[System.Serializable]
+
+
+
 public class NetworkConnect : MonoBehaviour
 {
-    public string joinCode;
-    public int maxConnection = 20;
-    public UnityTransport transport;
 
-    private async void Awake()
+
+    public void Create()
     {
-        await UnityServices.InitializeAsync();
-        await AuthenticationService.Instance.SignInAnonymouslyAsync();
-    }
-
-    public async void Create()
-    {
-        Allocation allocation = await RelayService.Instance.CreateAllocationAsync(maxConnection);
-        string newJoinCode = await RelayService.Instance.GetJoinCodeAsync(allocation.AllocationId);
-
-        Debug.LogError(newJoinCode);
-
-        transport.SetHostRelayData(allocation.RelayServer.IpV4, (ushort)allocation.RelayServer.Port,
-            allocation.AllocationIdBytes, allocation.Key, allocation.ConnectionData);
         NetworkManager.Singleton.StartHost();
     }
 
-    public async void Join()
+    public void Join()
     {
-        JoinAllocation allocation = await RelayService.Instance.JoinAllocationAsync(joinCode);
-        transport.SetClientRelayData(allocation.RelayServer.IpV4, (ushort)allocation.RelayServer.Port,
-            allocation.AllocationIdBytes, allocation.Key, allocation.ConnectionData, allocation.HostConnectionData);
-
         NetworkManager.Singleton.StartClient();
     }
 }
