89bbc007fe76f118a61d128bc428d33bef7d7386
AmethystApples <yakseenkhan@gmail.com>
2023-12-07 23:01:11 -0800

    Edited Pieces to be interactable. Continue implementing Netcode

--- imd-board-game-VR/Assets/NetworkConnect.cs
+++ imd-board-game-VR/Assets/NetworkConnect.cs
@@ -2,20 +2,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Unity.Netcode;
-
-
-
+using Unity.Services.Authentication;
+using Unity.Services.Core;
+using Unity.Services.Relay;
+using Unity.Services.Relay.Models;
+using Unity.Netcode.Transports.UTP;
+[System.Serializable]
 public class NetworkConnect : MonoBehaviour
 {
+    public string joinCode;
+    public int maxConnection = 20;
+    public UnityTransport transport;
 
-
-    public void Create()
+    private async void Awake()
     {
+        await UnityServices.InitializeAsync();
+        await AuthenticationService.Instance.SignInAnonymouslyAsync();
+    }
+
+    public async void Create()
+    {
+        Allocation allocation = await RelayService.Instance.CreateAllocationAsync(maxConnection);
+        string newJoinCode = await RelayService.Instance.GetJoinCodeAsync(allocation.AllocationId);
+
+        Debug.LogError(newJoinCode);
+
+        transport.SetHostRelayData(allocation.RelayServer.IpV4, (ushort)allocation.RelayServer.Port,
+            allocation.AllocationIdBytes, allocation.Key, allocation.ConnectionData);
         NetworkManager.Singleton.StartHost();
     }
 
-    public void Join()
+    public async void Join()
     {
+        JoinAllocation allocation = await RelayService.Instance.JoinAllocationAsync(joinCode);
+        transport.SetClientRelayData(allocation.RelayServer.IpV4, (ushort)allocation.RelayServer.Port,
+            allocation.AllocationIdBytes, allocation.Key, allocation.ConnectionData, allocation.HostConnectionData);
+
         NetworkManager.Singleton.StartClient();
     }
 }
